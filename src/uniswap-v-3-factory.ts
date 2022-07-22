a8ad50d6fbf071d217cea11568d055d0282b7b52
Richa-iitr <richa@cs.iitr.ac.in>
2022-07-22 06:45:26 +0530

    graph init

--- src/uniswap-v-3-factory.ts
+++ src/uniswap-v-3-factory.ts
@@ -1,15 +1,11 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   UniswapV3Factory,
-  FeeAmountEnabled,
-  OwnerChanged,
   PoolCreated
-} from "../generated/UniswapV3Factory/UniswapV3Factory"
-import { ExampleEntity } from "../generated/schema"
+} from "../generated/Factory/UniswapV3Factory"
+import { Pool } from "../generated/schema"
 
-export function handleFeeAmountEnabled(event: FeeAmountEnabled): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
+export function handleFeeAmountEnabled(event: PoolCreated): void {
   let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
