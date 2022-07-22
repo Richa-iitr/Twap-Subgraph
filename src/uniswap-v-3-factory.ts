e1b6c58a1981c86bf902a949f70e1903af72e051
Richa-iitr <richa@cs.iitr.ac.in>
2022-07-22 06:48:38 +0530

    Revert "graph init"
    This reverts commit a8ad50d6fbf071d217cea11568d055d0282b7b52.

--- src/uniswap-v-3-factory.ts
+++ src/uniswap-v-3-factory.ts
@@ -1,11 +1,15 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
   UniswapV3Factory,
+  FeeAmountEnabled,
+  OwnerChanged,
   PoolCreated
-} from "../generated/Factory/UniswapV3Factory"
-import { Pool } from "../generated/schema"
+} from "../generated/UniswapV3Factory/UniswapV3Factory"
+import { ExampleEntity } from "../generated/schema"
 
-export function handleFeeAmountEnabled(event: PoolCreated): void {
+export function handleFeeAmountEnabled(event: FeeAmountEnabled): void {
+  // Entities can be loaded from the store using a string ID; this ID
+  // needs to be unique across all entities of the same type
   let entity = ExampleEntity.load(event.transaction.from.toHex())
 
   // Entities only exist after they have been saved to the store;
